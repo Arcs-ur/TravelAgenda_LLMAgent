4e20ea6565b8b96c07c28d390ab0775ccc06961b
arcsur <arcs-ur@foxmail.com>
2024-10-13 20:51:46 +0800

    add databses and update readme

--- TravelAgenda/agenda/models.py
+++ TravelAgenda/agenda/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+#agenda的database里面要包括日程的名字比如“2024北京行”，要包括日程的内容。
+class agenda(models.Model):
+    name = models.CharField(max_length=100)
+    content = models.TextField(max_length=2000)
+
+    def __str__(self):
+        return self.name
--- TravelAgenda/posts/models.py
+++ TravelAgenda/posts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
+#这部分的需求是面向对象记录，对象是帖子，数据库的内容要包括：1.帖子的发布者，2.帖子的内容，3，帖子的发布时间，4.帖子的点赞数
+from django.contrib.auth import get_user_model
 
-# class DiaryPost(models.Model):
-#     title = models.CharField(max_length=255)
-#     content = models.TextField()
-#     location = models.CharField(max_length=255)
-#     date = models.DateField()
-#     image = models.ImageField(upload_to='diary_images/')
-#     created_at = models.DateTimeField(auto_now_add=True)
-    
-#     def __str__(self):
-#         return self.title
+class Post(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)  # 发布者
+    content = models.TextField()  # 帖子的内容
+    created_at = models.DateTimeField(auto_now_add=True)  # 帖子的发布时间
+    likes = models.PositiveIntegerField(default=0)  # 帖子的点赞数
+
+    def __str__(self):
+        return f"Post by {self.user.username} at {self.created_at}"
