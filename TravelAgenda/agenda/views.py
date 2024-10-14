7f65cd70f3c50022c05a9003a12bea4c1dc01182
arcsur <arcs-ur@foxmail.com>
2024-10-14 14:46:04 +0800

    agenda update

--- TravelAgenda/agenda/views.py
+++ TravelAgenda/agenda/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from .models import Agenda
+# def agenda_main(request):
+#     return render(request, 'agenda/main.html')
 
 def agenda_main(request):
-    return render(request, 'agenda/main.html')
+    agendas = Agenda.objects.all()  # 获取所有 Agenda 实例
+    return render(request, 'agenda/main.html', {'agendas': agendas})
 
 def agenda_calendar(request):
     return render(request, 'agenda/calendar.html')
