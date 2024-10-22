cbf05c05e956ebc45e9b966cac468c646dc74a7c
zsz <bernadoltom@gmail.com>
2024-10-22 14:59:23 +0800

    zsz实现destinations的部分html

--- TravelAgenda/destinations/urls.py
+++ TravelAgenda/destinations/urls.py
@@ -4,9 +4,9 @@ from django.contrib.auth import views as auth_views
 app_name = 'destinations'
 urlpatterns = [
     path('', views.destination_main, name='main'),
-    path('hotel', views.destination_hotel, name='hotel'),
-    path('restaurant', views.destination_restaurant, name='restaurant'),
-    path('shop', views.destination_shop, name='shop'),
+    path('hotel/', views.destination_hotel, name='hotel'),
+    path('restaurant/', views.destination_restaurant, name='restaurant'),
+    path('shop/', views.destination_shop, name='shop'),
     path('attraction/', views.destination_attraction, name='attraction'),
     path('traffic', views.destination_traffic, name='traffic'),
 ]
--- TravelAgenda/destinations/views.py
+++ TravelAgenda/destinations/views.py
@@ -7,51 +7,113 @@ def destination_main(request):
 
 # 酒店视图
 def destination_hotel(request):
-    return render(request, 'destinations/hotel.html')
-
-# 餐厅视图
-def destination_restaurant(request):
-    return render(request, 'destinations/restaurant.html')
-
-# 商店视图
-def destination_shop(request):
-    return render(request, 'destinations/shop.html')
-
-# 景点视图
-# def destination_attraction(request):
-#     return render(request, 'destinations/attraction.html')
-
-def destination_attraction(request):
     # 获取查询参数
     district_query = request.GET.get('district', '')  # 获取district参数
     stars_query = request.GET.get('stars', '')  # 获取stars参数
-    print(district_query)
-    print(stars_query)
     
-    # 获取所有目的地的查询集
-    destinations = Destination.objects.all()
+    # 获取必住的目的地
+    destinations = Destination.objects.filter(tags='HOTEL')
 
     # 如果提供了district查询参数，则过滤目的地
     if district_query:
         destinations = destinations.filter(address__icontains=district_query)
     
-
     # 如果提供了stars查询参数，则过滤目的地
     if stars_query:
         try:
             stars = int(stars_query)
-            destinations = destinations.filter(stars__gte=stars)
+            destinations = destinations.filter(stars__exact=stars)
         except ValueError:
             pass  # 处理非数字的stars输入
-    
-    
-    print(destinations)
 
     # 将过滤后的目的地列表传递给模板
     context = {
         'destinations': destinations,
     }
+    return render(request, 'destinations/hotel.html', context)
+
+# 餐厅视图
+def destination_restaurant(request):
+    # 获取查询参数
+    district_query = request.GET.get('district', '')  # 获取district参数
+    stars_query = request.GET.get('stars', '')  # 获取stars参数
+    
+    # 获取必吃的目的地
+    destinations = Destination.objects.filter(tags='EAT')
+
+    # 如果提供了district查询参数，则过滤目的地
+    if district_query:
+        destinations = destinations.filter(address__icontains=district_query)
+    
+    # 如果提供了stars查询参数，则过滤目的地
+    if stars_query:
+        try:
+            stars = int(stars_query)
+            destinations = destinations.filter(stars__exact=stars)
+        except ValueError:
+            pass  # 处理非数字的stars输入
+
+    # 将过滤后的目的地列表传递给模板
+    context = {
+        'destinations': destinations,
+    }
+    return render(request, 'destinations/restaurant.html', context)
+
+# 商场视图
+def destination_shop(request):
+    # 获取查询参数
+    district_query = request.GET.get('district', '')  # 获取district参数
+    stars_query = request.GET.get('stars', '')  # 获取stars参数
+    
+    # 获取必逛的目的地
+    destinations = Destination.objects.filter(tags='VISIT')
+
+    # 如果提供了district查询参数，则过滤目的地
+    if district_query:
+        destinations = destinations.filter(address__icontains=district_query)
+    
+    # 如果提供了stars查询参数，则过滤目的地
+    if stars_query:
+        try:
+            stars = int(stars_query)
+            destinations = destinations.filter(stars__exact=stars)
+        except ValueError:
+            pass  # 处理非数字的stars输入
+
+    # 将过滤后的目的地列表传递给模板
+    context = {
+        'destinations': destinations,
+    }
+    return render(request, 'destinations/shop.html', context)
+
+# 景点视图
+def destination_attraction(request):
+    # 获取查询参数
+    district_query = request.GET.get('district', '')  # 获取district参数
+    stars_query = request.GET.get('stars', '')  # 获取stars参数
+    
+    # 获取必玩的目的地
+    destinations = Destination.objects.filter(tags='PLAY')
+
+    # 如果提供了district查询参数，则过滤目的地
+    if district_query:
+        destinations = destinations.filter(address__icontains=district_query)
+    
+    # 如果提供了stars查询参数，则过滤目的地
+    if stars_query:
+        try:
+            stars = int(stars_query)
+            destinations = destinations.filter(stars__exact=stars)
+        except ValueError:
+            pass  # 处理非数字的stars输入
+    
+    # 将过滤后的目的地列表传递给模板
+    context = {
+        'destinations': destinations,
+    }
     return render(request, 'destinations/attraction.html', context)
+
+
 
 class AttractionListView(ListView):
     model = Destination
